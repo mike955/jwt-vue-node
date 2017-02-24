326d343bf97f6bd86ebefd8ecd43c4895ca06e06
zza00000 <superbear@24.com>
2017-02-24 09:16:28 +0800

    将vue实例从服务器渲染到页面

--- app.js
+++ app.js
@@ -2,13 +2,28 @@
 
 import path from 'path';
 import express from 'express';
+import Vue from 'vue';
+import {createRenderer} from 'vue-server-renderer';
 
 const port = process.env.PORT || 3000;
 const app = express();
+const renderer = createRenderer();
 
+
+const vm = new Vue({
+  template: '<div id=app>world</div>'
+});
 
 app.get('*', (req, res) => {
-  res.send('Hello');
+  renderer.renderToString(vm, (err, html) => {
+    if (err) {
+      console.error(err);
+      return res.send('Server Error')
+    }
+
+    console.log(html);
+    res.send(html);
+  });
 })
 
 app.listen(port, (err) => {
